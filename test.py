73c8ffcad37b9a81655778a5739e2bcc50c6855f
frederic <amihaconsulting@gmail.com>
2018-02-16 14:44:38 +0100

    update

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-cle
+# -*- coding: utf-8 -*-
+
+#test
+
+listing = [   
+]
+
+
+for v in listing:
+    name = "\"" + v + ' brython' + "\"" 
+    name_low = "\"" + v.lower() + "\"" 
+    name_min = "\"" + v + "\"" 
+    r = name +  ":" + "{" +  '"prefix"' + ":" +\
+           name_low  + "," + '"body"' +\
+        ":" + "[" + name_min  + "]," +\
+        '"description"' + ":" + name_min + "},"
+    print(r)    
+
